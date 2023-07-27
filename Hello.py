efc6fc27e5349aac7ce1d274a9c41bf2422bed71
Kshitij kumar singh <kshitijkumarsingh58@gmail.com>
2023-07-27 23:13:37 +0530

    Second commit

--- Hello.py
+++ Hello.py
@@ -62,3 +62,76 @@
 #          print("you are a minor")
 # else:
 #     print("you are a child")
+
+# #Calculator program
+# first=input("enter 1st number")
+# operator=input("enter operator(+,-,*,/,%): ")
+# second=input("enter 2nd number")
+# if operator == "+" :
+#     print(int(first) + int(second))
+# elif operator == "-" :
+#     print(int(first) - int(second))
+# elif operator == "*" :
+#     print(int(first) * int(second))
+# elif operator == "/" :
+#     print(int(first) / int(second))
+# elif operator == "%" :
+#     print(int(first) % int(second))
+# else:
+#     print("Invalid")
+ #while loop
+# i = 1
+# while i<=5:
+#      print(i)
+#      i=i+1
+
+#for loop
+# for i in range(5)
+# print(i)
+
+#List
+# marks=[67,78,90]
+# print(marks)
+# #for specific item
+# marks=[67,78,90]
+# print(marks[0])
+# print(marks[0:2])
+#  for score in marks:
+#      print(score)
+# marks.append() // to add value at the
+# marks.insert(0,89)//to add value at specific position
+# marks=[67,78,90]
+# i=0
+# while i<len(marks):
+#     print(marks[i])
+#     i=i+1
+# marks.clear()//to clear list
+
+#Break
+# students=["kshitij","kumar","ram","sita"]
+# for student in students
+#  if student=="ram":
+#      break;
+#  print(student)
+
+
+#Continue
+# students=["kshitij","kumar","ram","sita"]
+# for student in students
+#  if student=="ram":
+#      continue;//list will print without ram
+#  print(student)
+
+#Tuples
+# makrs=(89,98,98,78,67)
+# marks.count(98)//gives count
+# marks.index(98)//gives index
+
+#Sets
+# marks={89,89,90,87}
+# print(marks)//prints only unique values
+
+#Dictionary
+# marks={"eng":89,"hindi":78}
+# print(marks["eng"])
+# marks["phy"]=90;//to add new value
