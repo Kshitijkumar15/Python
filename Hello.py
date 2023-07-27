b7f113827c348d48087284c7d540326af55d59ed
Kshitij kumar singh <kshitijkumarsingh58@gmail.com>
2023-07-27 22:00:55 +0530

    Initial  commit

---
+++ Hello.py
@@ -0,0 +1,64 @@
+# #Basics
+
+# print("hello world")
+# name="kshitij"
+# age=21
+# print(name)
+# print(age)
+# is_Adult=True
+# print(is_Adult)
+
+# #Taking user input
+
+# name=input("Enter yor name ")
+# print(name)
+# old_age=input("enter your age")
+# new_age=int(old_age)+2
+# print(new_age)
+
+#Simple program
+
+# first=input("enter 1 num")
+# second=input("Enter second num")
+# sum=int(first)+int(second)
+# print(sum)
+
+
+#Strings
+# name="kshitij"
+# print(name.upper())
+# print(name.lower())
+# print(name.find('s'))
+# print(name.replace('kshitij','kumar'))
+# print('k'in name)
+
+# #Operators
+# print(5+2)
+# print(5-2)
+# print(5*2)
+# print(5/2) #can be float value 
+# print(5//2) #gives only int value
+# print(5**2)#power to
+# print(3>2)
+# print(3<2)
+# print(3>=2)
+# print(3<=2)
+# print(3==2)
+# print(3!=2)
+
+#Logical operator
+# print(2>3 or 2>1) #true
+# print(3>2 and 2<1) #true
+# print(not 2>3) #true
+
+
+#If-else
+# age=21
+# if age>=18:
+#     print("you are eligible")
+#     print("you can vote")
+#     print("thank you")
+# elif age<18 and age>3:
+#          print("you are a minor")
+# else:
+#     print("you are a child")
